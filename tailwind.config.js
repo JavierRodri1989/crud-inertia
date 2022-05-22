22c7d7032e5eb51ea41fa28b613e2e272471ce6e
[pruebas] Javier Rodriguez <javier1989rodri@gmail.com>
2022-05-22 22:38:12 +0200

    Modelo y migración de category y pongo en tailwind para que el container este centrado por defecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
 
     theme: {
         extend: {
+            container: {
+                center: true
+            },
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
